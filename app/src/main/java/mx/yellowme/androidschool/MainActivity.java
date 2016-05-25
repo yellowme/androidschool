a5c86e0462d4c30b8e14affc23e27762d711090c
Javier Olan <javikin10@gmail.com>
2016-05-25 16:34:40 -0500

    base project

--- app/src/main/java/mx/yellowme/androidschool/MainActivity.java
+++ app/src/main/java/mx/yellowme/androidschool/MainActivity.java
@@ -8,19 +8,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textViewUsuario;
-    private EditText editTextUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewUsuario = (TextView) findViewById(R.id.textViewUsuario);
-        editTextUsuario = (EditText) findViewById(R.id.editTextUsuario);
     }
 
-    public void setValue(View view){
-        String cadena = editTextUsuario.getText().toString();
-        editTextUsuario.setText(cadena);
-    }
+
 }
