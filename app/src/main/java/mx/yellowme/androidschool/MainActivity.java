6a12bfa3d9e836770b45f6e49da7d8f43779b1ab
Javier Olan <javikin10@gmail.com>
2016-05-25 16:33:50 -0500

    solved 6

--- app/src/main/java/mx/yellowme/androidschool/MainActivity.java
+++ app/src/main/java/mx/yellowme/androidschool/MainActivity.java
@@ -2,12 +2,25 @@ package mx.yellowme.androidschool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewUsuario;
+    private EditText editTextUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewUsuario = (TextView) findViewById(R.id.textViewUsuario);
+        editTextUsuario = (EditText) findViewById(R.id.editTextUsuario);
+    }
+
+    public void setValue(View view){
+        String cadena = editTextUsuario.getText().toString();
+        editTextUsuario.setText(cadena);
     }
 }
